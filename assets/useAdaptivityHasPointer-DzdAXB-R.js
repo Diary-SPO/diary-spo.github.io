4cf0f15b081522a0f222dad5fb76de68f8d66e33
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-03-31 20:51:43 +0700

    Updates

---
+++ assets/useAdaptivityHasPointer-DzdAXB-R.js
@@ -0,0 +1,1 @@
+import{P as t,e as s,aX as i}from"./index-BqqhbCth.js";import{a5 as o}from"./index-D82QvMrw.js";function f(n=!0){const{hasPointer:e}=t(s);return!o(!(n||e===void 0))||e!==void 0?e:i}export{f as u};
